3490dc01cdac2444fb176160d3d8bf0e88f10429
malecki (Dr ) <malecki@gmail.com>
2011-05-04 23:04:56 +0000

    template from memisc.ext for lmer objects
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/apsrtable@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/apsrtable.R
+++ pkg/R/apsrtable.R
@@ -858,3 +858,50 @@ pfround <- function (x, digits) {
 ## A couple of test calls here for random features
 ## library(apsrtable);example(apsrtable);apsrtable(lm.D90, lm.D9, glm.D9, digits=1, align="center", stars="default", model.counter=0, order="rl", omitcoef="(Intercept)")
 ## library(apsrtable);example(apsrtable);apsrtable(lm.D90,coef.names=c("\\#1","\\#0"))
+
+
+## setSummaryTemplate(mer = c("Log-likelihood" = "($logLik:f#)",
+##                      "Deviance" = "($deviance:f#)",
+##                      "AIC" = "($AIC:f#)",
+##                      "BIC" = "($BIC:f#)",
+##                      "N" = "($N:d)",
+##                      "Groups" = "($Groups:d)"))
+
+## getSummary.mer <- function (obj, alpha = 0.05, ...) {
+##   smry <- summary(obj)
+##   coef <- smry@coefs
+##   lower <- qnorm(p = alpha/2, mean = coef[, 1], sd = coef[,2])
+##   upper <- qnorm(p = 1 - alpha/2, mean = coef[, 1], sd = coef[,2])
+##   if (ncol(smry@coefs) == 3) {
+##     p <- (1 - pnorm(smry@coefs[, 3])) * 2
+##     coef <- cbind(coef, p, lower, upper)
+##   }
+##   else {
+##     coef <- cbind(coef, lower, upper)
+##   }
+##   RE <- smry@REmat
+##   ranef <- cbind(as.numeric(RE[,3]), as.numeric(RE[,4]), NA,NA,NA,NA)
+##   rownames(ranef) <- paste("Ranef", RE[,1], sep = " - ")
+##   coef <- rbind(coef, ranef)
+##   colnames(coef) <- c("est", "se", "stat", "p", "lwr", "upr")
+
+##   ## Factor levels.
+##   xlevels <- list()
+##   Contr <- names(attr(model.matrix(obj), "contrasts"))
+##   for (c in Contr) xlevels[[c]] <- levels(obj@frame[,c])
+
+##   ## Model fit statistics.
+##   ll <- logLik(obj)[1]
+##   deviance <- deviance(obj)
+##   AIC <- AIC(obj)
+##   BIC <- BIC(obj)
+##   N <- as.numeric(smry@dims["n"])
+##   G <- as.numeric(smry@ngrps)
+##   sumstat <- c(logLik = ll, deviance = deviance, AIC = AIC,
+##                BIC = BIC, N = N, Groups = G)
+
+##   ## Return model summary.
+##   list(coef = coef, sumstat = sumstat,
+##        contrasts = attr(model.matrix(obj), "contrasts"),
+##        xlevels = xlevels, call = obj@call)
+## }
